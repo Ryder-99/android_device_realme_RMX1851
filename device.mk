cd7fbb74be62ee2f884ce2f3fabc2541b2558892
LuK1337 <priv.luk@gmail.com>
2019-09-10 19:02:00 +0200

    RMX1851: Copy up to date handheld_core_hardware to system
    * Vendor one is outdated and therefore we lose stuff
      like secure lockscreen...
    
    Change-Id: I458867b3b322ad754b78877209b7f4d8fb78eb38

--- device.mk
+++ device.mk
@@ -30,6 +30,10 @@ DEVICE_PACKAGE_OVERLAYS += \
 # Properties
 -include $(LOCAL_PATH)/props.mk
 
+# Permissions
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml
+
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
